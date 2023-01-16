384260b2f34c45a1083ddc3b33a5b648bea74b21
Ashutosh Kewat <109691178+Ashu6200@users.noreply.github.com>
2023-01-16 16:35:35 +0530

    Update Api.js

--- client/src/context/Api.js
+++ client/src/context/Api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com"
 const options = {
     params: { hl: 'en', gl: 'US' },
     headers: {
-        'X-RapidAPI-Key': '9f07655f6emshd2ecc204ee38da8p120e6djsn6de4574ea6b5',
+        'X-RapidAPI-Key': 
         'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
     }
 };
